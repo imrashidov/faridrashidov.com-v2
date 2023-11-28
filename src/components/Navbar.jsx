48fac7e8e8ba76d7dfbc045ec7e606f8e29d3ec7
Farid Rashidov <farikrashidov@gmail.com>
2023-11-29 03:25:46 +0400

    Adding pseudo-elements on hover (navbar items)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,36 +9,36 @@ import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <div className="nav-main flex justify-between items-center px-60 w-full py-4 ">
+    <div className="nav-main flex justify-between items-center px-60 w-full py-3 ">
       <a href="/" className="flex-shrink-0 ">
-        <img className="nav-logo w-40 " src={logo} alt="Logo" />
+        <img className="nav-logo w-48" src={logo} alt="Logo" />
       </a>
-      <div className="nav-items flex gap-24 ">
-        <div className="nav-item">
-          <NavLink to="/" className="nav-item-navlink">
+      <div className="nav-items relative flex gap-24  ">
+        <div className="nav-item   ">
+          <NavLink to="/" className="nav-item-navlink ">
             <RiHome3Line />
             Home
           </NavLink>
         </div>
-        <div className="nav-item">
+        <div className="nav-item  ">
           <NavLink to="/about" className="nav-item-navlink">
             <GoPerson />
             About
           </NavLink>
         </div>
-        <div className="nav-item">
+        <div className="nav-item  ">
           <NavLink to="/projects" className="nav-item-navlink">
             <AiOutlineFundProjectionScreen /> Projects
           </NavLink>
         </div>
-        <div className="nav-item">
+        <div className="nav-item ">
           <NavLink to="/resume" className="nav-item-navlink">
             <IoDocumentOutline /> Resume
           </NavLink>
         </div>
         <div className="nav-github">
           <a href="https://github.com/imrashidov">
-            <FaGithub className="w-10 h-10 text-violet-900 " />
+            <FaGithub className="w-10 h-10 text-violet-900  rounded-full hover:shadow-spec hover:shadow-violet-600" />
           </a>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
       fontFamily: {
         raleway: ["Raleway", "arial"],
       },
+      boxShadow: {
+        spec: "0px 0px 5px 5px rgba(0 0 0 / 0.05)",
+      },
     },
   },
   plugins: [],
