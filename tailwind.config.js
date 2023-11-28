20f743e316715fd22a27b63ae210730d9a3dcebd
Farid Rashidov <farikrashidov@gmail.com>
2023-11-28 19:15:35 +0400

    Adding google fonts & deleting default css properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple1: "#1b1429",
+        purple2: "#140F23",
+      },
+      fontFamily: {
+        raleway: ["Raleway", "arial"],
+      },
+    },
   },
   plugins: [],
 };
