32cde274b5f671aadb9cc93bb3de849554ed6762
Farid Rashidov <farikrashidov@gmail.com>
2023-11-28 20:02:06 +0400

    Adding Routes and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/Projects.jsx";
+import Resume from "./components/Resume.jsx";
+import ErrorElement from "./components/ErrorElement.jsx";
+import Footer from "./components/Footer.jsx";
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<ErrorElement />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
