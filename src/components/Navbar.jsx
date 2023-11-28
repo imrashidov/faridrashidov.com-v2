2a477abe0dc7e6bc26fae3c449dc99f5e641fd0b
Farid Rashidov <farikrashidov@gmail.com>
2023-11-28 21:30:28 +0400

    Adding nav-items and logo on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav-main flex justify-between items-center px-60 w-full py-4 ">
+      <a href="/" className="flex-shrink-0 ">
+        <img className="nav-logo w-40 " src={logo} alt="Logo" />
+      </a>
+      <div className="nav-items flex gap-32">
+        <div className="nav-item">
+          <NavLink to="/">Home</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink to="/about">About</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink to="/projects">Projects</NavLink>
+        </div>
+        <div className="nav-item">
+          <NavLink to="/resume">Resume</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
