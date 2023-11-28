b0d94ea7d8fba4ced747faa2e4fba542a0787d2b
Farid Rashidov <farikrashidov@gmail.com>
2023-11-28 21:59:17 +0400

    Installing react-icons and icons on nav-items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { RiHome3Line } from "react-icons/ri";
+import { GoPerson } from "react-icons/go";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { IoDocumentOutline } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
 import logo from "../assets/logo.png";
 
 export default function Navbar() {
@@ -8,18 +13,33 @@ export default function Navbar() {
       <a href="/" className="flex-shrink-0 ">
         <img className="nav-logo w-40 " src={logo} alt="Logo" />
       </a>
-      <div className="nav-items flex gap-32">
+      <div className="nav-items flex gap-24 ">
         <div className="nav-item">
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" className="nav-item-navlink">
+            <RiHome3Line />
+            Home
+          </NavLink>
         </div>
         <div className="nav-item">
-          <NavLink to="/about">About</NavLink>
+          <NavLink to="/about" className="nav-item-navlink">
+            <GoPerson />
+            About
+          </NavLink>
         </div>
         <div className="nav-item">
-          <NavLink to="/projects">Projects</NavLink>
+          <NavLink to="/projects" className="nav-item-navlink">
+            <AiOutlineFundProjectionScreen /> Projects
+          </NavLink>
         </div>
         <div className="nav-item">
-          <NavLink to="/resume">Resume</NavLink>
+          <NavLink to="/resume" className="nav-item-navlink">
+            <IoDocumentOutline /> Resume
+          </NavLink>
+        </div>
+        <div className="nav-github">
+          <a href="https://github.com/imrashidov">
+            <FaGithub className="w-10 h-10 text-violet-900 " />
+          </a>
         </div>
       </div>
     </div>
