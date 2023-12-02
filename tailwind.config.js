7fb3dfba3cfed86624524fedc2018427e3a7edec
Farid Rashidov <farikrashidov@gmail.com>
2023-12-02 19:13:18 +0400

    coding wave animation to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,24 @@ export default {
       boxShadow: {
         spec: "0px 0px 5px 5px rgba(0 0 0 / 0.05)",
       },
+      transformOrigin: {
+        custom: "70% 70%",
+      },
+      animation: {
+        wave: "wave 2.1s infinite ",
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10deg)" },
+          "60%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(0deg)" },
+        },
+      },
     },
   },
   plugins: [],
