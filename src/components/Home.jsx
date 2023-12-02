ca11e261986f5521bb509eec420a0e530f36de53
Farid Rashidov <farikrashidov@gmail.com>
2023-12-02 18:43:04 +0400

    Installing typewriter-effect and creating function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Type from "./Type.jsx";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section>
+      <div className="home"></div>
+      <div className="about"></div>
+    </section>
+  );
 }
---
+++ src/components/Type.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: ["Front-End Developer", "MERN Stack Developer", "Freelancer"],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
